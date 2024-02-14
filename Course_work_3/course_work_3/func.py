4f97761865ce43aaa145e11d1a226b5b6740d8a8
Elvina <Elle-elle7@yandex.ru>
2024-02-14 22:02:54 +0500

    add test_func.py, change func.py, create __init__.py

--- Course_work_3/course_work_3/func.py
+++ Course_work_3/course_work_3/func.py
@@ -1,4 +1,3 @@
-import datetime
 import json
 from datetime import datetime
 
---
+++ Course_work_3/tests/test_func.py
@@ -0,0 +1,53 @@
+from Course_work_3.course_work_3.func import *
+
+
+src = {'id': 782295999, 'state': 'EXECUTED', 'date': '2019-09-11T17:30:34.445824',
+       'operationAmount': {'amount': '54280.01', 'currency': {'name': 'USD', 'code': 'USD'}},
+       'description': 'Перевод организации', 'from': 'Счет 24763316288121894080', 'to': 'Счет 96291777776753236930'}
+
+
+def test_get_currency():
+    assert get_currency(src) == 'USD'
+
+
+def test_get_amount():
+    assert get_amount(src) == '54280.01'
+
+
+def test_get_mask_receiver_account_number():
+    assert get_mask_receiver_account_number('96291777776753236930') == '**6930'
+    assert get_mask_receiver_account_number('962917777753237745') == '**7745'
+
+
+def test_get_receiver_account():
+    assert get_receiver_account(src) == 'Счет 96291777776753236930'
+
+
+def test_split_sender_account_number():
+    assert split_sender_account_number('247633**********4080') == '2476 33** **** **** 4080'
+
+
+def test_get_mask_sender_account_number():
+    assert get_mask_sender_account_number('24763316288121894080') == '247633**********4080'
+
+
+def test_get_number_account():
+    assert get_number_account('Счет 24763316288121894080') == '24763316288121894080'
+    assert get_number_account('Счет 96291777776753236930') == '96291777776753236930'
+
+
+def test_get_name_account():
+    assert get_name_account('Счет 24763316288121894080') == 'Счет'
+    assert get_name_account('Счет 96291777776753236930') == 'Счет'
+
+
+def test_get_description():
+    assert get_description(src) == 'Перевод организации'
+
+
+def test_get_formatted_date():
+    assert get_formatted_date(src) == '11.09.2019'
+
+
+def test_get_sender_account():
+    assert get_sender_account(src) == 'Счет 24763316288121894080'
