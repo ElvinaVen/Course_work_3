c0aa11923f762deca6165d50a13b9dd89c0c752e
Elvina <Elle-elle7@yandex.ru>
2024-02-14 19:54:13 +0500

    fix 14/02/2024

--- Course_work_3/course_work_3/func.py
+++ Course_work_3/course_work_3/func.py
@@ -2,108 +2,184 @@ import datetime
 import json
 from datetime import datetime
 
-OPERATIONS_SOURCE = "operations.json"
+
+def load_transactions_from_json(TRANSACTIONS_SOURCE):
+    """
+    Функция чтения файла json
+    :param TRANSACTIONS_SOURCE: "operations.json" - файл, откуда считываем
+    :return: transactions_list - список всех транзакций из файла
+    """
+    with open(TRANSACTIONS_SOURCE, 'r', encoding='utf-8') as file:
+        transactions_list = json.load(file)  # загрузка из файла
+    return transactions_list
 
 
-def load_operations_from_json():  # ф-ия чтения файла json
-    with open(OPERATIONS_SOURCE, 'r', encoding='utf-8') as file:
-        operations_list = json.load(file)  # загрузка из файла
-    return operations_list
-
-
-def get_executed_operations(operation_list):  # ф-ия формирования нового списка только из выполненных операций
+def get_executed_transactions(transactions_list):
+    """
+    Функция формирования нового списка только из выполненных транзакций
+    :param transactions_list: список всех транзакций из файла
+    :return: executed_state_list - список транзакций со статусом "EXECUTED"
+    """
     executed_state_list = []  # создаем пустой список
-    for operation in operation_list:
-        if operation.get('state') == "EXECUTED":  # если статус выполнен
-            executed_state_list.append(operation)  # формируем новый список только из выполненных операций
+    for transaction in transactions_list:
+        if transaction.get('state') == "EXECUTED":  # если статус "EXECUTED"
+            executed_state_list.append(transaction)  # формируем новый список только из выполненных транзакций
     return executed_state_list
 
 
-def sort_data(executed_state_list):  # сортируем список
-    # выполненных операций по убыванию даты
+def get_sort_list(executed_state_list):
+    """
+    Функция сортировки списка транзакций по убыванию даты
+    :param executed_state_list: список транзакций со статусом "EXECUTED"
+    :return: transactions_sort_list - список транзакций сортированный по дате убывания
+    """
     executed_state_list.sort(key=lambda operation: datetime.fromisoformat(operation.get('date')), reverse=True)  #
     # сортировка
-    sort_list = executed_state_list  # создаем новый сортированный список
-    return sort_list
+    transactions_sort_list = executed_state_list  # создаем новый сортированный список
+    return transactions_sort_list
 
 
-def get_five_operations(sort_list):  # ф-ия получения последних пяти выполненных операций клиента
-    last_operations_list = sort_list[0:5]
-    return last_operations_list
+def get_last_transactions(transactions_sort_list, TRANSACTION_COUNT):
+    """
+    Функция получения последних выполненных транзакций клиента
+    :param transactions_sort_list: список транзакций сортированный по дате убывания
+    :param TRANSACTION_COUNT: количество транзакций, которые необходимо вывести
+    :return: selected_transactions_list - отобранный список транзакций
+    """
+    selected_transactions_list = transactions_sort_list[0:TRANSACTION_COUNT]
+    return selected_transactions_list
 
 
-def get_formatted_date(one_operation):  # ф-ия получения форматированной даты из словаря
-    date = datetime.strptime(one_operation['date'], "%Y-%m-%dT%H:%M:%S.%f")  # разбираем значение даты по ключу
+def get_formatted_date(transaction):
+    """
+    Функция получения форматированной даты из транзакции
+    :param transaction: единичная транзакция
+    :return: date_formatted - отформатированная дата в виде дд.мм.гггг.
+    """
+    date = datetime.strptime(transaction['date'], "%Y-%m-%dT%H:%M:%S.%f")  # разбираем значение даты по ключу
     date_formatted = date.strftime("%d.%m.%Y")  # форматируем в нужном нам формате дд.мм.гггг.
     return date_formatted
 
 
-def get_description(one_operation):  # ф-ия получения описания из словаря операции
-    description = one_operation['description']
+def get_description(transaction):
+    """
+     Функция получения описания транзакции
+    :param transaction: единичная транзакция
+    :return: description - описание транзакции
+    """
+    description = transaction['description']
     return description
 
 
-def get_from_account(one_operation):
-    from_account = one_operation.get('from')
-    return from_account
+def get_sender_account(transaction):
+    """
+    Функция получения счета отправителя в формате "Visa Classic 2842878893689012"
+    :param transaction: единичная транзакция
+    :return: sender_account - счет отправителя
+    """
+    sender_account = transaction.get('from')
+    return sender_account
 
 
-def get_from_account_alpha(from_account):
-    just_alpha = []
-    from_account = str(from_account).split(" ")
-    for word in from_account:
-        if word.isalpha():
-            just_alpha.append(word)
-    return ' '.join(just_alpha)
+def get_name_account(account):
+    """
+    Функция получения имени счета
+    :param account: счет отправителя\получателя
+    :return: pay_system_name - тип платежной системы отправителя\получателя
+    """
+    name_account = []
+    account_string = str(account).split(" ")  # разделяем строку на подстроки по пробелам
+    for word in account_string:
+        if word.isalpha():  # если это слово, то добавляем в список name_account
+            name_account.append(word)
+    pay_system_name = ' '.join(name_account)  # объединяем подстроки в строку через пробел
+    return pay_system_name
 
 
-def get_from_account_digit(from_account):
+def get_number_account(account):
+    """
+    Функция получения номера счета
+    :param account: счет отправителя\получателя
+    :return: number_account - номер счета отправителя\получателя
+    """
     just_digit = []
-    from_account = str(from_account).split(" ")
-    for word in from_account:
-        if word.isdigit():
+    account_string = str(account).split(" ")  # разделяем строку на подстроки по пробелам
+    for word in account_string:
+        if word.isdigit():  # если это цифра, то добавляем в список just_digit
             just_digit.append(word)
-    return ' '.join(just_digit)
+    number_account = ' '.join(just_digit)  # объединяем подстроки в строку через пробел
+    return number_account
 
 
-def mask_number_account(split_result_account):
-    a = len(split_result_account)
-    result_account = ''
+def get_mask_sender_account_number(sender_account_number):
+    """
+    Функция маскировки номера счета отправителя
+    :param sender_account_number: номер счета отправителя
+    :return: masked_sender_account - замаскированный номер счета отправителя в формате "284287******9012"
+    """
+    a = len(sender_account_number)
+    masked_sender_account = ''
     for i in range(a):
-        if i >= 6 and i < (a-4):
-            result_account += "*"
+        if (i >= 6) and (i < (a - 4)):
+            masked_sender_account += "*"
         else:
-            result_account += split_result_account[i]
-    return result_account
+            masked_sender_account += sender_account_number[i]
+    return masked_sender_account
 
 
-def split_number_account(result_account):
-    a = len(result_account)
-    split_result_account = [result_account[i:i+4] for i in range(0, a, 4)]
-    split_result_account2 =' '.join(split_result_account)
+def split_sender_account_number(masked_sender_account):
+    """
+    Функция разделения номера счета на 4 части
+    :param masked_sender_account: замаскированный номер счета отправителя в формате "284287******9012"
+    :return: sender_account_number - результирующий номер счета отправителя
+    """
+    a = len(masked_sender_account)
+    split_sender_account = [masked_sender_account[i:i + 4] for i in range(0, a, 4)]
+    sender_account_number = ' '.join(split_sender_account)  # объединяем подстроки в строку через пробел
+    return sender_account_number
 
-    return split_result_account2
+
+def get_receiver_account(transaction):
+    """
+    Функция получения счета получателя
+    :param transaction: единичная транзакция
+    :return: receiver_account - счет получателя
+    """
+    receiver_account = transaction.get('to')
+    return receiver_account
 
 
-def get_to_account(one_operation):
-    to_account = one_operation.get('to')
-    return to_account
-
-
-def mask_number_to_account(digit_to):
-    a = digit_to[-6:]
-    result_account_to = ''
+def get_mask_receiver_account_number(receiver_account_number):
+    """
+    Функция маскировки номера счета получателя
+    :param receiver_account_number: номер счета получателя
+    :return: masked_receiver_account - замаскированный номер счета получателя в формате "**5907"
+    """
+    a = receiver_account_number[-6:]
+    masked_receiver_account = ''
     for i in range(len(a)):
         if i < 2:
-            result_account_to += "*"
+            masked_receiver_account += "*"
         else:
-            result_account_to += digit_to[i]
-    return result_account_to
+            masked_receiver_account += a[i]
+    return masked_receiver_account
 
 
-def get_operation_amount_amount(one_operation):
-    return one_operation["operationAmount"]["amount"]
+def get_amount(transaction):
+    """
+    Функция получения суммы перевода
+    :param transaction: единичная транзакция
+    :return: amount - сумма перевода
+    """
+    amount = transaction["operationAmount"]["amount"]
+    return amount
 
 
-def get_operation_amount_name(one_operation):
-    return one_operation["operationAmount"]["currency"]["name"]
+def get_currency(transaction):
+    """
+    Функция получения валюты
+    :param transaction: единичная транзакция
+    :return: currency - валюта
+    """
+    currency = transaction["operationAmount"]["currency"]["name"]
+    return currency
--- Course_work_3/course_work_3/main.py
+++ Course_work_3/course_work_3/main.py
@@ -1,30 +1,34 @@
-from func import get_executed_operations, get_five_operations, sort_data, load_operations_from_json, get_formatted_date
-from func import get_description, get_from_account_alpha, get_from_account_digit, get_from_account, get_operation_amount_name
-from func import mask_number_account, split_number_account, get_to_account, mask_number_to_account, get_operation_amount_amount
+from func import *
 
-operations_list = load_operations_from_json()  # запуск ф-ии чтения файла json
-executed_state_list = get_executed_operations(
-    operations_list)  # запуск ф-ии формирования нового списка только из выполненных операций
-sort_list = sort_data(executed_state_list)  # запуск ф-ии сортировки списка выполненных операций по убыванию даты
-last_operations_list = get_five_operations(sort_list)
+TRANSACTIONS_SOURCE = "operations.json"
+TRANSACTION_COUNT = 8
 
-for one_operation in last_operations_list:
-    date_formatted = get_formatted_date(one_operation)  # вызов ф-ии получения форматированной даты из словаря операции
-    description = get_description(one_operation)  # вызов ф-ии получения описания из словаря операции
-    from_account = get_from_account(one_operation)
-    alpha = get_from_account_alpha(from_account)
-    digit = get_from_account_digit(from_account)
-    result_account = mask_number_account(digit)
-    split_result_account = split_number_account(result_account)
-    to_account = get_to_account(one_operation)
+transactions_list = load_transactions_from_json(TRANSACTIONS_SOURCE)  # запуск ф-ии чтения файла json
+transactions_sort_list = get_sort_list(get_executed_transactions(
+    transactions_list))  # запуск ф-ий формирования нового списка только из выполненных операций и его дальнейшей
+# сортировки по убыванию даты
+selected_transactions_list = get_last_transactions(transactions_sort_list, TRANSACTION_COUNT)  # запуск ф-ии отбора
+# определенного количества последних транзакций из сортированного списка
 
-    alpha_to = get_from_account_alpha(to_account)
-    digit_to = get_from_account_digit(to_account)
-    result_account_to = mask_number_to_account(digit_to)
-    amount = get_operation_amount_amount(one_operation)  #!!!!!
-    name = get_operation_amount_name(one_operation)
+for transaction in selected_transactions_list:
+    date_formatted = get_formatted_date(transaction)  # вызов ф-ии получения форматированной даты из транзакции
+    description = get_description(transaction)  # вызов ф-ии получения описания из транзакции
+
+    sender_account = get_sender_account(transaction)  # вызов ф-ии получения счета отправителя
+    sender_account_name = get_name_account(sender_account)  # вызов ф-ии получения типа платежной системы
+    masked_sender_account = get_mask_sender_account_number(get_number_account(sender_account))  # вызов ф-ии маскировки
+    # номер счета отправителя
+    sender_account_number = split_sender_account_number(masked_sender_account)  # вызов ф-ии разделения номера счета
+    # на 4 части
+
+    receiver_account = get_receiver_account(transaction)  # вызов ф-ии получения счета получателя
+    receiver_account_name = get_name_account(receiver_account)  # вызов ф-ии получения типа платежной системы
+    masked_receiver_account = get_mask_receiver_account_number(get_number_account(receiver_account))  # вызов ф-ии
+    # маскировки номера счета получателя
+
+    amount = get_amount(transaction)  # вызов ф-ии получения суммы перевода
+    currency = get_currency(transaction)  # вызов ф-ии получения валюты
 
     print(f"{date_formatted} {description}\n"
-          f"{alpha} {split_result_account} -> {alpha_to} {result_account_to}\n"
-          f"{amount} {name}")
-    print()
+          f"{sender_account_name} {sender_account_number} -> {receiver_account_name} {masked_receiver_account}\n"
+          f"{amount} {currency}\n")
